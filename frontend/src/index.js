444008ab869607baa7d27eabda8be6bbc6865743
daferali20 <31751340+daferali20@users.noreply.github.com>
2025-05-23 01:16:07 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error("لم يتم العثور على عنصر root في index.html");
-}
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
