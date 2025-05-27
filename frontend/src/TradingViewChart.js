0d7933bf5e0b06071c8f4fa62ebbda0f19beb973
daferali20 <31751340+daferali20@users.noreply.github.com>
2025-05-28 01:58:11 +0300

    Update TradingViewChart.js

--- frontend/src/TradingViewChart.js
+++ frontend/src/TradingViewChart.js
@@ -5,7 +5,7 @@ function TradingViewChart({ symbol }) {
 
   return (
     <div style={{ marginTop: '2rem' }}>
-      <h3>📉 الشارت اللحظي لسهم {symbol}</h3>
+      <h3>📉 الشارت اللحظي لسهم {TSLA}</h3>
       <div style={{ height: '500px', width: '100%' }}>
         <iframe
           src={`https://s.tradingview.com/widgetembed/?symbol=NASDAQ:${symbol}&interval=5&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=F1F3F6&studies=[]&theme=light&style=1&timezone=Etc%2FUTC&withdateranges=1&hideideas=1&hide_side_toolbar=1`}
