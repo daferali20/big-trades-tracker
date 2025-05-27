3bf07b901872cada2c1067a5ee2f396176aad405
daferali20 <31751340+daferali20@users.noreply.github.com>
2025-05-27 23:10:29 +0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    host: true, // مهم لدعم التشغيل على Render
-    port: 3000,
-  },
+  plugins: [react()]
 })
