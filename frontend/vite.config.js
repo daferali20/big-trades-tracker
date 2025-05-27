3b761ec275d7d4438000bc2822d033799b528178
daferali20 <31751340+daferali20@users.noreply.github.com>
2025-05-27 22:59:33 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // تحديد جذر المشروع
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'dist',
   },
-});
+  server: {
+    host: true, // مهم لدعم التشغيل على Render
+    port: 3000,
+  },
+})
